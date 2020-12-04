ad829d8e23fec620dfba9c8ec7fec316ee8de5a4
llanos <llanos1205@gmail.com>
2020-12-04 17:16:50 -0400

    flow update

--- Compute/dbrestore.sh
+++ Compute/dbrestore.sh
@@ -1,1 +1,15 @@
 #!bin/bash
+git clone
+psql postgresql://<user>:<password>@<host>/<db> << EOF 
+     CREATE DATABASE CoreDB;
+     CREATE DATABASE Client_Template;
+     CREATE USER testUser PASSWORD 'copaclacome'
+
+EOF 
+
+pg_restore -h $1 -U $2 -W -d CoreDB -f CiCdAwsDeployments/Compute/backuptesis -F c -O -s << EOF
+altair1205 
+EOF
+pg_restore -h $1 -U $2 -W -d Client_Template -f CiCdAwsDeployments/Compute/backuptesis -F c -O -s << EOF
+altair1205 
+EOF
